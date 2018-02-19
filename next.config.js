8a3f3183c26d6f4361c82cafc8c81f30396f514e
Jason Tu <jasontu4@gmail.com>
2018-02-19 16:56:56 -0500

    Install next-typescript

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withTypeScript = require('@zeit/next-typescript');
+
+module.exports = withTypeScript({
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -7,4 +9,4 @@ module.exports = {
 
     return config
   }
-}
+});
