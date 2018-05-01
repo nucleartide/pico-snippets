b6c275619c419959cf0518bfa2e9e60c21ed7031
Jason Tu <jasontu4@gmail.com>
2018-05-01 06:54:45 -0400

    Add gravy

---
+++ gravy.lua
@@ -0,0 +1,20 @@
+
+--
+-- gravy.
+--
+
+gravy = {}
+gravy.__index = gravy
+
+function gravy.new(pos, gravity, max_dy)
+  self.pos     = pos
+  self.gravity = gravity or 0.15
+  self.max_dy  = max_dy  or 2
+  self.dy      = 0
+end
+
+function gravy:update()
+  self.dy    += self.gravity
+  self.dy    =  mid(-self.max_dy, self.dy, self.max_dy)
+  self.pos.y += dy
+end
