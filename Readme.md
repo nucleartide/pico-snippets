305d9fc40f5b3f4b88087f4e7f5fce482f539c4f
Jason Tu <jasontu4@gmail.com>
2018-05-01 05:34:40 -0400

    Delete overengineered app idea

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# snippets
+
+Drop-in snippets for your [PICO-8](https://www.lexaloffle.com/pico-8.php) game.
--- client/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import Hello from './components/Hello';
-
-ReactDOM.render(<Hello name="Bob" />, document.getElementById('app'));
