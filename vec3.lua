8122e38fffd1c03069d310bd143769c008b787c6
Jason Tu <jasontu4@gmail.com>
2018-05-22 08:27:19 -0400

    Update vec3.lua

--- vec3.lua
+++ vec3.lua
@@ -3,7 +3,22 @@
 -- vec3 util.
 --
 
-vec3 = {}
+vec3 = {
+  __add = function(a, b)
+    a.x += b.x
+    a.y += b.y
+    a.z += b.z
+    return a
+  end,
+
+  __sub = function(a, b)
+    a.x -= b.x
+    a.y -= b.y
+    a.z -= b.z
+    return a
+  end,
+}
+
 vec3.__index = vec3
 
 function vec3.new(x, y, z)
@@ -15,17 +30,26 @@ function vec3.new(x, y, z)
 end
 
 function vec3:magnitude()
-  return sqrt(self.x^2 + self.y^2)
+  return sqrt(self.x^2 + self.y^2 + self.z^2)
 end
 
 function vec3:normalize()
   local m = self:magnitude()
-  if (m == 0) return
+  if m == 0 then return end
   self.x /= m
   self.y /= m
   self.z /= m
+  return self
 end
 
-function vec3:debug()
+function vec3:tostr()
   return self.x .. ',' .. self.y .. ',' .. self.z
+end
+
+function vec3.cross(a, b)
+  local ax, ay, az = a.x, a.y, a.z
+  a.x = ay*b.z - az*b.y
+  a.y = az*b.x - ax*b.z
+  a.z = ax*b.y - ay*b.x
+  return a
 end
